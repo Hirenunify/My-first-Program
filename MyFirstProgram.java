2c7764686bcf320ba2f6474da14708ad85dbdc19
Hiren Bhatt <hirenbhatt2511@gmail.com>
2022-07-19 20:56:22 +0100

    my first java program

---
+++ MyFirstProgram.java
@@ -0,0 +1,10 @@
+package Javaconcepts;
+
+public class MyFirstProgram {
+
+    public static void main(String[] args)  {
+        System.out.println("My First Application");
+        System.out.println("Hiren Bhatt");
+        System.out.println("QA at Unify");
+    }
+}
